57a50ccd07b86cd57dff76941ad0fbf94e0fd495
XuanMclane <xuanmclane@gmail.com>
2019-05-17 00:29:18 -0400

    finished LengthOfLongestSubstring

--- LengthOfLongestSubstring.cs
+++ LengthOfLongestSubstring.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Collections.Generic;
 
 public class LengthOfLongestSubstring 
 {
     public int solution(string s) 
     {
-        return 1;
+        int ans = 0;
+        int i = 0;
+        int j = 0;
+        HashSet<char> charSet = new HashSet<char>();
+        int n = s.Length;
+        while(i < n && j < n) 
+        {
+            if(!charSet.Contains(s[j]))
+            {
+                charSet.Add(s[j]);
+                j++;
+                ans = Math.Max(ans, j - i);
+            }
+            else
+            {
+                charSet.Remove(s[i]);
+                i++;
+            }
+        }
+        return ans;
     }
 }
