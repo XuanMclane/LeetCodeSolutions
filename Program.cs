dfa9b78e28188b8bffe4f4ecb3ff97d0b62645b4
XuanMclane <xuanmclane@gmail.com>
2019-05-17 23:11:46 -0400

    ready to go

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LeetCodeSolutions
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      LengthOfLongestSubstring l = new LengthOfLongestSubstring();
+      int output = l.Solution("pwwkdgegegegaadgegadaageafdsvadsvewdddefe");
+      Console.WriteLine(output);
+    }
+  }
+}
